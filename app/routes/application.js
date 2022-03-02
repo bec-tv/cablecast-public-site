63ba5ae43002b681c8491c96c12dd81d5a2e5293
Tyler Berkshire <tyler.berkshire@cablecast.tv>
2022-03-02 12:09:25 -0500

    fastboot logs

--- app/routes/application.js
+++ app/routes/application.js
@@ -104,13 +104,16 @@ export default class ApplicationRoute extends Route {
   }
 
   async model(params) {
+    console.log(this.get('fastboot.isFastBoot'));
+    console.log(this.get('fastboot.request.headers'));
+
     let host = '';
     if (this.get('fastboot.isFastBoot')) {
       let headers = this.get('fastboot.request.headers');
       host = headers.get('x-ccs-host');
     }
     else {
-      host = this.window.location.host;
+      //host = this.window.location.host;
     }
     
     let base = ENV.CCSServer;
