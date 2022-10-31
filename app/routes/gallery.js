655f49d490e07e1d0fe82e8a72a5361e9bf7adbc
Ray Tiley <raytiley@gmail.com>
2022-10-31 08:35:56 -0400

    CC-3401 - Fix some endpoints pagination

--- app/routes/gallery.js
+++ app/routes/gallery.js
@@ -3,6 +3,8 @@ import Route from '@ember/routing/route';
 import ENV from 'cablecast-public-site/config/environment';
 import { inject as service } from '@ember/service';
 
+const PAGE_SIZE=50;
+
 @classic
 export default class GalleryRoute extends Route {
   @service
@@ -15,9 +17,13 @@ export default class GalleryRoute extends Route {
   };
 
   async model(params) {
-    let offset = params.page - 1;
+    let page = params.page ?? 1;
+    let offset = 0;
+    if (page > 1) {
+      offset = (page - 1) * PAGE_SIZE;
+    }
     let api = this.get('api');
-    let result = await api.fetch(`api/publicsitedata/galleries/${params.id}`, {offset: offset});
+    let result = await api.fetch(`api/publicsitedata/galleries/${params.id}`, {offset: offset, page_size: PAGE_SIZE});
     let json = await result.json();
     
     return json;
